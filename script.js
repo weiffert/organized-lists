04dfbbbc6b30b8751a662f21f2148901549ac9fc
weiffert <weiffert@gmail.com>
2018-05-17 16:36:16 -0400

    Move items in array

--- script.js
+++ script.js
@@ -19,13 +19,13 @@ const app = {
 
     renderListItem(data) {
         const li = this.template.cloneNode(true);
-        
+
         this.myAddEventListener(li, 'button.star', 'click', 'handleStar');
         this.myAddEventListener(li, 'button.rename', 'click', 'handleRename');
         this.myAddEventListener(li, 'button.up', 'click', 'handleUp');
         this.myAddEventListener(li, 'button.down', 'click', 'handleDown');
         this.myAddEventListener(li, 'button.delete', 'click', 'handleDelete');
-        
+
         li.classList.remove('template');
         li.dataset.id = data.id;
         li
@@ -59,28 +59,34 @@ const app = {
     handleStar(event) {
         const li = event.target.parentElement.parentElement;
         li.classList.toggle('favorite');
-        
+
         this.hikes.forEach(hike => {
-            if(hike.id === parseInt(li.dataset.id))
+            if (hike.id === parseInt(li.dataset.id))
                 hike.favorite = !hike.favorite;
         });
     },
 
     handleDown(event) {
-        try {
-            const li = event.target.parentElement.parentElement;
-            this.list.insertBefore(li, li.nextSibling.nextSibling);
-        } catch(error) {
+        const li = event.target.parentElement.parentElement;
+        const index = this.hikes.findIndex(hike => hike.id === parseInt(li.dataset.id));
 
+        if (index < this.hikes.length - 1) {
+            this.list.insertBefore(li, li.nextSibling.nextSibling);
+
+            const value = this.hikes.splice(index, 1);
+            this.hikes.splice(index + 1, 0, value[0]);
         }
     },
 
     handleUp(event) {
-        try {
         const li = event.target.parentElement.parentElement;
-        this.list.insertBefore(li, li.previousSibling);
-        } catch(error) {
+        const index = this.hikes.findIndex(hike => hike.id === parseInt(li.dataset.id));
 
+        if (index > 0) {
+            this.list.insertBefore(li, li.previousSibling);
+
+            const value = this.hikes.splice(index, 1);
+            this.hikes.splice(index - 1, 0, value[0]);
         }
     },
 
