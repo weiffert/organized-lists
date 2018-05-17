ae42701fe2cffce076313c2019d00fbffc501740
weiffert <weiffert@gmail.com>
2018-05-17 11:29:27 -0400

    Add id to list item's dataset

--- script.js
+++ script.js
@@ -14,6 +14,7 @@ const app = {
 
     renderListItem(data) {
         const li = document.createElement('li');
+        li.dataset.id = data.id;
         li.appendChild(renderPElement(data.name));
         li.appendChild(renderButtonList());
         return li;
